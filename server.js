f95ec235538a2c56cfc346459aa00bc056ad09ae
taibiayub <taibiayub@outlook.com>
2019-05-25 01:50:34 -0800

    Second commit, route files created

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+//connect db
+connectDB();
+
 app.get('/',(req, res)=>res.send('API Running'));
+//define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/post',require('./routes/api/post'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`server started on port ${PORT}`));
